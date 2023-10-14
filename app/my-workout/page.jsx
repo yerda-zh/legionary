8cfd0684f14da80df57f905acb0887ec0ad06717
yerda-zh <yerdaulet.04@gmail.com>
2023-10-15 03:19:58 +0600

    improved how api response is displayed

--- app/my-workout/page.jsx
+++ app/my-workout/page.jsx
@@ -3,13 +3,32 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 
 export default function MyWorkout() {
-  const routine = useSelector((state) => state.routine);
+  const response = useSelector((state) => state.routine);
   const [loading, setLoading] = useState(true);
-
+  // if (response !== null) {
+  //   setLoading(false);
+  // }
   return (
     <div>
-      <pre>{routine === null ? <p>loading</p> : routine}</pre>
-      {console.log(routine)}
+      {response === null ? (
+        <p>loading...</p>
+      ) : (
+        <div>
+          <h3>Introduction</h3>
+          <p>{response.introduction}</p>
+          <h3>Equipment needed</h3>
+          {response.equipment.map((item) => (
+            <p>{item}</p>
+          ))}
+          <h3>Workout response</h3>
+          <h4>Monday</h4>
+          {response.routine.mon.map((item) => (
+            <p>{item}</p>
+          ))}
+          <h3>Advice</h3>
+          <p>{response.advice}</p>
+        </div>
+      )}
     </div>
   );
 }
