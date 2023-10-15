1f71429ff4e5da96f248e903d818a93f1119ffe9
yerda-zh <yerdaulet.04@gmail.com>
2023-10-15 12:21:07 +0600

    working on mapping over weekdays

--- app/my-workout/page.jsx
+++ app/my-workout/page.jsx
@@ -1,34 +1,79 @@
 "use client";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useState } from "react";
 
 export default function MyWorkout() {
   const response = useSelector((state) => state.routine);
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
   const [loading, setLoading] = useState(true);
-  // if (response !== null) {
-  //   setLoading(false);
-  // }
-  return (
-    <div>
-      {response === null ? (
-        <p>loading...</p>
-      ) : (
+
+  useEffect(() => {
+    if (response !== null) {
+      setLoading(false);
+    }
+  }, [response]);
+
+  if (loading) {
+    return <p>loading...</p>;
+  } else {
+    return (
+      <div>
         <div>
           <h3>Introduction</h3>
           <p>{response.introduction}</p>
+        </div>
+        <div>
           <h3>Equipment needed</h3>
           {response.equipment.map((item) => (
-            <p>{item}</p>
+            <p key={item}>{item}</p>
           ))}
+        </div>
+        <div>
           <h3>Workout response</h3>
-          <h4>Monday</h4>
-          {response.routine.mon.map((item) => (
-            <p>{item}</p>
+          {days.map((day) => (
+            <h4 key={day}>{day}</h4>
           ))}
+          {response.routine.mon.map((item) => (
+            <p key={item}>{item}</p>
+          ))}
+        </div>
+        <div>
           <h3>Advice</h3>
           <p>{response.advice}</p>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
+
+  // return (
+  //   <div>
+  //     {response === null ? (
+  //       <p>loading...</p>
+  //     ) : (
+  //       <div>
+  //         <h3>Introduction</h3>
+  //         <p>{response.introduction}</p>
+  //         <h3>Equipment needed</h3>
+  //         {response.equipment.map((item) => (
+  //           <p>{item}</p>
+  //         ))}
+  //         <h3>Workout response</h3>
+  //         <h4>Monday</h4>
+  //         {response.routine.mon.map((item) => (
+  //           <p>{item}</p>
+  //         ))}
+  //         <h3>Advice</h3>
+  //         <p>{response.advice}</p>
+  //       </div>
+  //     )}
+  //   </div>
+  // );
 }
