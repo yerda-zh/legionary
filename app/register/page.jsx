249510431383f7e4fa07aa5dcd133b33cf4c9c70
yerda-zh <yerdaulet.04@gmail.com>
2023-10-20 23:55:22 +0600

    connected to database endpoints signin and register

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 
 export default function Register() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
   const [pass1, setPass1] = useState("");
@@ -22,25 +22,60 @@ export default function Register() {
     setPass2(event.target.value);
   };
   const onSubmitRegister = () => {
-    if(pass1 === pass2) {
-        setPassword(pass1);
-        console.log(name, email, password);
+    if (pass1 === pass2 && pass1.length !== 0) {
+      const password = pass1;
+      fetch("http://localhost:5000/register", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((answers) => {
+          console.log(answers);
+        })
+        .catch(console.log);
     }
-  }
+  };
 
   return (
     <div>
       <h1>Register</h1>
-      <div>
+      <form>
         <p>Name</p>
-        <input placeholder="Enter Name" type="text" onChange={onNameChange} required />
+        <input
+          placeholder="Enter Name"
+          type="text"
+          onChange={onNameChange}
+          required
+        />
         <p>Email</p>
-        <input placeholder="Enter Email" type="email" onChange={onEmailChange} required />
+        <input
+          placeholder="Enter Email"
+          type="email"
+          onChange={onEmailChange}
+          required
+        />
         <p>Password</p>
-        <input placeholder="Enter Password" type="password" onChange={onPass1Change} required />
-        <input placeholder="Enter Password Again" type="password" onChange={onPass2Change} required />
-        <button type="submit" onClick={onSubmitRegister}>Register</button>
-      </div>
+        <input
+          placeholder="Enter Password"
+          type="password"
+          onChange={onPass1Change}
+          required
+        />
+        <input
+          placeholder="Enter Password Again"
+          type="password"
+          onChange={onPass2Change}
+          required
+        />
+        <button type="submit" onClick={onSubmitRegister}>
+          Register
+        </button>
+      </form>
     </div>
   );
 }
--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -2,18 +2,43 @@
 import { useState } from "react";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+
+  const onSubmitSignIn = () => {
+    fetch("http://localhost:5000/signin", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        }),
+      })
+        .then((res) => res.json())
+        .then((answers) => {
+          console.log(answers);
+        })
+        .catch(console.log);
+  }
 
   return (
     <div>
       <h1>Sign In</h1>
-      <div>
+      <form>
         <p>Email</p>
-        <input placeholder="Enter Email" type="email" required />
+        <input placeholder="Enter Email" type="email" onChange={onEmailChange} required />
         <p>Password</p>
-        <input placeholder="Enter Password" type="password" required />
-        <button type="submit">Sign In</button>
-      </div>
+        <input placeholder="Enter Password" type="password" onChange={onPasswordChange} required />
+        <button type="submit" onClick={onSubmitSignIn}>Sign In</button>
+      </form>
     </div>
   );
 }
