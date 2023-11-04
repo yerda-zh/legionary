5c6df037471ca9c5482d71f38f416d2167317a6b
yerda-zh <yerdaulet.04@gmail.com>
2023-11-04 23:41:52 +0600

    added animations, worked in navigation bar

--- app/_components/navbar/Navbar.jsx
+++ app/_components/navbar/Navbar.jsx
@@ -1,11 +1,29 @@
 "use client";
-import { NavContainer, FirstDiv, SecondDiv, UserP } from "./navbar.styles";
+import {
+  NavContainer,
+  FirstDiv,
+  SecondDiv,
+  SmallScreenContainer,
+  MenuIcon,
+  SmallScreen,
+  CloseIcon,
+  SmallScreenLinks,
+} from "./navbar.styles";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import "../../_animations/animations.scss";
 
 const Navbar = () => {
   const user = useSelector((state) => state.user);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   const router = useRouter();
+
+  const handleMenuLinks = (route) => {
+    setToggleMenu(false);
+    router.push(route);
+  };
 
   return (
     <NavContainer>
@@ -15,11 +33,32 @@ const Navbar = () => {
       <SecondDiv>
         <p onClick={() => router.push("/my-workout")}>My Workout Plan</p>
         {user.id ? (
-          <p className="username" onClick={() => router.push("/profile")}>{user.name}</p>
+          <p className="username" onClick={() => router.push("/profile")}>
+            {user.name}
+          </p>
         ) : (
           <p onClick={() => router.push("/signin")}>Log In</p>
         )}
       </SecondDiv>
+
+      <SmallScreenContainer>
+        <MenuIcon onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <SmallScreen className={`${toggleMenu && "navbar-slide-left"}`}>
+            <CloseIcon onClick={() => setToggleMenu(false)} />
+            <SmallScreenLinks>
+              <li><p onClick={() => handleMenuLinks("/my-workout")}>My Workout Plan</p></li>
+              <li>
+                {user.id ? (
+                  <p className="username" onClick={() => handleMenuLinks("/profile")}>{user.name}</p>
+                ) : (
+                  <p onClick={() => handleMenuLinks("/signin")}>Log In</p>
+                )}
+              </li>
+            </SmallScreenLinks>
+          </SmallScreen>
+        )}
+      </SmallScreenContainer>
     </NavContainer>
   );
 };
--- app/_components/navbar/navbar.styles.jsx
+++ app/_components/navbar/navbar.styles.jsx
@@ -1,27 +1,88 @@
 'use client'
 import { styled } from "styled-components";
+import { HiMenuAlt3 } from 'react-icons/hi';
+import {RxCross2} from 'react-icons/rx';
 
 export const NavContainer = styled.nav`
     background: var(--color-main);
+    color: var(--color-base);
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 1rem 2rem;
-    color: var(--color-base);
+    p {
+        cursor: pointer;
+    }
 `;
 export const FirstDiv = styled.div`
     p {
         font-weight: bold;
-        cursor: pointer;
     }
 `;
 export const SecondDiv = styled.div`
     display: flex;
-    p {
-        margin-left: 2rem;
-        cursor: pointer;
-    }
+    gap: 2rem;
+    
     .username {
         color: var(--color-accent);
+    }
+
+    @media screen and (max-width: 576px) {
+        display: none;
+    }
+`;
+
+export const SmallScreenContainer = styled.div`
+    display: none;
+    color: var(--color-white);
+
+    .username {
+        color: var(--color-accent);
+    }
+    
+    @media screen and (max-width: 576px) {
+        display: flex;
+    }
+`;
+
+export const SmallScreen = styled.div`
+    z-index: 1;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background: var(--color-main);
+    transition: .5s ease;
+`;
+
+export const MenuIcon = styled(HiMenuAlt3)`
+    font-size: 1.6rem;
+
+`;
+export const CloseIcon = styled(RxCross2)`
+    font-size: 1.6rem;
+
+    position: absolute;
+    top: 1rem;
+    right: 1.2rem;
+`;
+export const SmallScreenLinks = styled.ul`
+    list-style: none;
+
+    li {
+        font-family: var(--font-base);
+        margin: 2rem;
+        cursor: pointer;
+        color: var(--color-white);
+        font-size: 1.2rem;
+        text-align: center;
+        
+        &:hover {
+            color: var(--color-l-blue);
+        }
     }
 `;
