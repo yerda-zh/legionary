197fc5bb43577445d464555c824c6926fe3f8805
yerda-zh <yerdaulet.04@gmail.com>
2023-11-08 01:32:45 +0600

    almost finished join section

--- app/_components/join/Join.jsx
+++ app/_components/join/Join.jsx
@@ -1,54 +1,78 @@
-'use client'
-import { JoinContainer } from "./join.styles";
-import Link
- from "next/link";
+"use client";
+import { JoinContainer, WhyContainer, FeaturesContainer, FeatureDiv, JoinDiv } from "./join.styles";
+import { useRouter } from "next/navigation";
+import {BsFillPersonFill} from 'react-icons/bs';
+import {BiSolidTimer} from 'react-icons/bi';
+import {AiFillTrophy} from 'react-icons/ai';
+import {PiTargetBold} from 'react-icons/pi';
+
 const Join = () => {
+  const router = useRouter();
+
   return (
     <JoinContainer>
-      <div>
+      <WhyContainer>
         <h2>Why Choose AI-Powered Workouts?</h2>
-        <div>
-          <div>
-            <h4>Personalizion</h4>
+        <FeaturesContainer>
+          <FeatureDiv>
+            <div>
+                <h4>Personalizion</h4>
+                <BsFillPersonFill className="featureLogo"/>
+            </div>
             <p>
               AI considers your unique needs, making your workouts more
               effective and enjoyable.
             </p>
-          </div>
-          <div>
-            <h4>Efficiency</h4>
+          </FeatureDiv>
+          <FeatureDiv>
+            <div>
+              <h4>Efficiency</h4>
+              <BiSolidTimer/>
+            </div>
             <p>
               Say goodbye to hours spent searching for the perfect routine. Our
               AI does the heavy lifting for you.
             </p>
-          </div>
-          <div>
-            <h4>Motivation</h4>
+          </FeatureDiv>
+          <FeatureDiv>
+            <div>
+              <h4>Motivation</h4>
+              <AiFillTrophy/>
+            </div>
+            
             <p>
               With ever-evolving workouts and achievable goals, you'll stay
               motivated and eager to hit the gym.
             </p>
-          </div>
-          <div>
-            <h4>Results</h4>
+          </FeatureDiv>
+          <FeatureDiv>
+            <div>
+              <h4>Results</h4>
+              <PiTargetBold/>
+            </div>
             <p>
               AI's precision ensures you get results faster, helping you achieve
               your fitness goals.
             </p>
-          </div>
+          </FeatureDiv>
+        </FeaturesContainer>
+      </WhyContainer>
+
+      <JoinDiv>
+        <h2>Join the Fitness Revolution</h2>
+        <p>
+          Get started today and experience the transformation for yourself.
+          Welcome to a new era of fitness where your goals are just a click
+          away, powered by the intelligence of tomorrow.
+        </p>
+        <div>
+          <button onClick={() => router.push("/create-workout")}>
+            Create a workout routine
+          </button>
         </div>
-      </div>
-      <h2>Join the Fitness Revolution</h2>
-      <p>
-        Get started today and experience the transformation for yourself.
-        Welcome to a new era of fitness where your goals are just a click away,
-        powered by the intelligence of tomorrow.
-      </p>
-      <Link href="/create-workout">
-        <button>Create a workout routine</button>
-      </Link>
+      </JoinDiv>
     </JoinContainer>
   );
-}
+};
 
 export default Join;
--- app/_components/join/join.styles.jsx
+++ app/_components/join/join.styles.jsx
@@ -1,7 +1,137 @@
 import styled from "styled-components";
 
 export const JoinContainer = styled.div`
+    background: var(--color-main);
+    color: var(--color-base);
+    padding: 2rem 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+export const WhyContainer = styled.div`
+    h2 {
+        color: var(--color-accent);
+        font-size: clamp(1rem, 2vw + 1rem, 2rem);
+        margin-bottom: 2rem;
+        text-align: center;
+    }
+`;
+
+export const FeaturesContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1.5rem;
+
+    @media screen and (max-width: 1064px){
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (max-width: 600px){
+        grid-template-columns: repeat(1, 1fr);
+    }
+`;
+
+export const FeatureDiv = styled.div`
+    background: var(--color-secondary);
+    padding: 2.5rem 1rem;
+    border-radius: 1rem;
+    transition: 0.2s ease-in-out;
+
+    div {
+        color: var(--color-accent-light);
+        font-size: clamp(1rem, 2vw + 1.3rem, 1.5rem);
+        border-bottom: 0.1rem solid var(--color-accent);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+        padding-bottom: 0.5rem;
+        margin-bottom: 1.5rem;
+    }
+
+    p {
+        font-size: clamp(0.5rem, 2vw + 1rem, 1.1rem);
+        line-height: 2rem;
+        text-align: justify;
+    }
+      
+    &:hover {
+        opacity: 0.7;
+        transform: scale(1.03);
+    }
+`;
+export const JoinDiv = styled.div`
+    background: var(--color-accent-light);
+    color: var(--color-main);
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 1rem;
+    border-radius: 0.5rem;
+
+    h2{
+        font-size: clamp(1rem, 2vw + 1rem, 2rem);
+        margin-bottom: 1rem;
+    }
+
+    p {
+        font-size: clamp(0.5rem, 2vw + 1rem, 1rem);
+        margin-bottom: 1rem;
+        text-align: center;
+    }
+
+    div {
+        width: 19rem;
+        height: 3rem;
+        border-radius: 1rem;
+        overflow: hidden;
+        border: none;
+        position: relative;
+        z-index: 1;
+        background: var(--color-main);
+    }
+
+    button {
+        border: 1.5px solid var(--color-main);
+        color: var(--color-base);
+        font-weight: 700;
+        font-size: clamp(0.7rem, 2vw + 0.5rem, 1.1rem);
+        border-radius: 1rem;
+        width: 100%;
+        height: 100%;
+        background-color: transparent;
+	    transition: 0.25s ease-in-out;
+        cursor: pointer;
+    }
+    span {
+        z-index: 1;
+    }
+
+    button::before {
+        content: "";
+        width: 0%;
+        height: 100%;
+        position: absolute;
+        background: var(--color-accent-light);
+        right: 0;
+        top: 0;
+        z-index: -1;
+        transition: 0.25s ease-in-out;
+    }
+    button:hover {
+        color: var(--color-main);
+        border-color: var(--color-main);
+    }
+    button:hover::before {
+        content: "";
+        width: 100%;
+        height: 100%;
+        position: absolute;
+    }
+
+    @media screen and (max-width: 575px) {
+        div {
+            width: 17rem;
+            align-self: center;
+        }
+    }
 `;
